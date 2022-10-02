910b2a77747fd370f055b5ae5e03c14b1bc14a63
Melina Lencina <melina00lencina@gmail.com>
2022-10-02 17:45:09 -0300

    :sparkles: More practice

--- src/components/TukiChild.js
+++ src/components/TukiChild.js
@@ -3,8 +3,13 @@ import { useState } from "react";
 const TukiChild = () => {
 
   const [count, setCount] = useState(0);
+  const [info, setInfo] = useState({ name: "Fabi", age: 28 });
+  const [loading, setLoading] = useState(false);
+  const [animals, setAnimals] = useState(["perro", "gato", "conejo", "pez"]);
 
-  console.log("TukiChild");
+  console.log("info", info);
+  console.log("loading", loading);
+  console.log("animals", animals);
 
   const increase = () => {
     console.log("Ahora increase funciona");
@@ -16,6 +21,20 @@ const TukiChild = () => {
     setCount(count - 1);
   };
 
+    const changeInfo = () => {
+      setInfo({ ...info, name: "Fabián", lastName: "Tureo", age: 29 });
+  };
+
+  const handleLoading = () => {
+    setLoading(!loading);
+  };
+
+  const animalsArray = ["elefante", "jirafa", "tigre", "leon"];
+
+  const addAnimal = () => {
+    setAnimals([...animals, ...animalsArray]);
+  };
+
     return (
       <div className="TukiChild">
         <h1>Tuki Child</h1>
@@ -23,6 +42,9 @@ const TukiChild = () => {
         <h3>Contador: {count}</h3>
         <button onClick={increase}>Aumentar</button>
         <button onClick={decrease}>Disminuir</button>
+        <button onClick={changeInfo}>Change info</button>
+        <button onClick={handleLoading}>Loading</button>
+        <button onClick={addAnimal}>Add animal</button>
       </div>
     );
 };
